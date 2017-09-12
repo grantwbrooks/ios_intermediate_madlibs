f279725d0e1650df90eca688a5be9841a953f9d6
grantwbrooks <grantwbrooks@gmail.com>
2017-09-12 15:14:06 -0400

    Initial Commit

---
+++ madlibs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  madlibs
+//
+//  Created by Grant Brooks on 9/12/17.
+//  Copyright © 2017 Grant Brooks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
