c1193db0f632a194b02949c3892d4742bb67435d
grantwbrooks <grantwbrooks@gmail.com>
2017-09-12 19:17:59 -0400

    finished madlibs

--- madlibs/ViewController.swift
+++ madlibs/ViewController.swift
@@ -8,11 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WordInputDelegate {
 
+    var sentenceWords: [String]?
+    var sentences: [String]?
+
+    
+    @IBOutlet weak var sentenceLabel: UILabel!
+    
+    
+    func addToArray(word: [String]) {
+        sentenceWords = word
+        print(sentenceWords!)
+        sentences = ["We are having a perfectly \(sentenceWords![0]) time now. Later we will \(sentenceWords![1]) and \(sentenceWords![2]) in the \(sentenceWords![3])", "We had a \(sentenceWords![0]) day. Next we will \(sentenceWords![1]) and \(sentenceWords![2]) to the \(sentenceWords![3])"]
+        sentenceLabel.text = String(describing: sentences![Int(arc4random_uniform(UInt32(sentences!.count)))])
+        dismiss(animated: true, completion: nil)
+
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let x = segue.destination as! WordInputTableViewController
+        x.delegate = self
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        sentenceLabel.text = "..."
     }
 
     override func didReceiveMemoryWarning() {
